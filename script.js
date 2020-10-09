9c60ceb2d5574899a7146e4569c784c6095d1786
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-09 00:21:11 -0400

    adding models in script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const video = document.getElementById('video');
+const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tnyFaceDetector.loadFromUri('')
-])
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
 function startVideo() {
   navigator.getUserMedia(
@@ -11,5 +14,3 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
-startVideo()
